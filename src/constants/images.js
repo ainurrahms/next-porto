c1dc902c1699eba29d73fa1ba26ee8e6f1d8f55f
Ainur Rahman <ainur.rahman@amartha.com>
2023-11-26 21:26:36 +0700

    feat: FEAT-002 Add my resume section, and quotes

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,15 @@
-import PP from "@assets/images/dummy-pp.png";
-import arrow from "@assets/images/arrow.svg";
+import PP from '@assets/images/dummy-pp.png';
+import arrow from '@assets/images/arrow.svg';
+import resume from '@assets/images/resume.jpg';
+import tablet from '@assets/images/tablet.png';
+import star from '@assets/images/star.svg';
 
 const images = {
   PP,
   arrow,
+  resume,
+  tablet,
+  star,
 };
 
 export default images;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,108 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import images from '@constants/images'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import images from '@constants/images';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`max-w-none w-full ${inter.className}`}
-    >
-      <section className="h-85vh bg-white bg-contain flex justify-center items-center bg-gradient-jumbotron">
-        <div className='padding-global'>
-          <div className='py-16'>
-            <div className='text-center flex flex-col justify-center items-center'>
-              <div className='w-32 h-32 bg-white border border-solid border-gray-300 rounded-full justify-center shadow-md items-center flex mb-6'>
-                <Image
-                  src={images.PP}
-                  alt="PP-Dummy"
-                  width={100}
-                  height={24}
-                  priority
-                />
+    <main className={`w-full max-w-none ${inter.className}`}>
+      <section className="bg-gradient-jumbotron flex h-85vh items-center justify-center bg-white bg-contain">
+        <div className="padding-global">
+          <div className="py-16">
+            <div className="flex flex-col items-center justify-center text-center">
+              <div className="mb-6 flex h-32 w-32 items-center justify-center rounded-full border border-solid border-gray-300 bg-white shadow-md">
+                <Image src={images.PP} alt="PP-Dummy" priority />
               </div>
-              <p className='font-bold text-5xl mb-6'>Hey, I&apos;m Ainur Rahman</p>
-              <p className='text-base opacity-60'>- Always Ambitious</p>
+              <p className="mb-6 text-5xl font-bold">Hey, I&apos;m Ainur Rahman</p>
+              <p className="text-base opacity-60">- Always Ambitious</p>
             </div>
           </div>
         </div>
       </section>
-      <section className='border-t-2 border-solid border-gray-300'>
-        <div className='padding-global'>
-          <div className='w-full max-w-7xl mx-auto'>
-            <div className='py-28'>
-              <div className="grid gap-x-20 gap-y-16 border-solid border-gray-300 rounded-lg grid-cols-2 grid-rows-auto auto-cols-fr items-start p-8">
-                <div className='flex gap-x-8 gap-y-8'>
-                  <p className='font-bold text-4xl'>A bit about me.</p>
-                  <Image
-                  className='mt-9'
-                    src={images.arrow}
-                    alt="Arrow"
-                    width={83}
-                    height={24}
-                    priority
-                  />
+      <section className="border-t-2 border-solid border-gray-300">
+        <div className="padding-global">
+          <div className="mx-auto w-full max-w-7xl">
+            <div className="py-28">
+              <div className="grid-rows-auto grid auto-cols-fr grid-cols-2 items-start gap-x-20 gap-y-16 rounded-lg border-solid border-gray-300 p-8">
+                <div className="flex gap-x-8 gap-y-8">
+                  <p className="text-4xl font-bold">A bit about me.</p>
+                  <Image className="mt-9" src={images.arrow} alt="Arrow" width={83} height={24} priority />
                 </div>
                 <div>
-                  <p className='text-base'>A person who love to solving problem with code. Currently passionate about FrontEnd things. Open to discussion about FrontEnd or out of it. Still learning about How JavaScript can be so strong in every platform and focusing learning about React one of the most powerfull library.</p>
+                  <p className="text-base">
+                    A person who love to solving problem with code. Currently passionate about FrontEnd things. Open to
+                    discussion about FrontEnd or out of it. Still learning about How JavaScript can be so strong in
+                    every platform and focusing learning about React one of the most powerfull library.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Resume */}
+      <section>
+        <div className="padding-global">
+          <div className="mx-auto w-full max-w-7xl">
+            <div className="pb-28 pt-14">
+              <div className="flex flex-col items-center">
+                <div className="w-full max-w-3xl">
+                  <div className="mb-6">
+                    <p className="text-center text-4xl font-bold">My Resume</p>
+                  </div>
+                </div>
+                <div className="resume">
+                  <div className="ml-[-4px] h-[790px] w-[1050px] overflow-scroll rounded-3xl border-0 bg-transparent">
+                    <Image src={images.resume} alt="resume" priority />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Describe */}
+      <section>
+        <div className="padding-global">
+          <div className="mx-auto w-full max-w-7xl">
+            <div className="py-28">
+              <div className="flex flex-col items-center">
+                <div className="mx-auto w-full max-w-3xl">
+                  <div className="mx-auto flex w-full max-w-3xl flex-col items-center justify-center">
+                    <div className="flex gap-1">
+                      <Image className="mt-9" src={images.star} alt="Arrow" width={20} height={20} priority />
+                      <Image className="mt-9" src={images.star} alt="Arrow" width={20} height={20} priority />
+                      <Image className="mt-9" src={images.star} alt="Arrow" width={20} height={20} priority />
+                      <Image className="mt-9" src={images.star} alt="Arrow" width={20} height={20} priority />
+                      <Image className="mt-9" src={images.star} alt="Arrow" width={20} height={20} priority />
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <div className="my-8 text-center">
+                        <p className="italic opacity-60">
+                          "Wahai orang-orang yang beriman, bertakwalah kepada Allah dan hendaklah setiap orang
+                          memperhatikan apa yang telah diperbuatnya untuk hari esok (akhirat). Bertakwalah kepada Allah.
+                          Sesungguhnya Allah Mahateliti terhadap apa yang kamu kerjakan."
+                        </p>
+                      </div>
+                      <p className="font-semibold">- (QS. Al-Hasyr 59:18)</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Projects */}
+      <section>
+        <div className="padding-global-projects">
+          <div className="mx-auto w-full max-w-7xl">
+            <div className="pb-28 pt-14">
+              <div className="mb-20">
+                <div className="w-full max-w-3xl">
+                  <p className="mb-6 text-5xl font-bold">Projects</p>
+                  <p className="text-xl opacity-60">Highlight of my best projects</p>
                 </div>
               </div>
             </div>
@@ -53,5 +110,5 @@ export default function Home() {
         </div>
       </section>
     </main>
-  )
+  );
 }
