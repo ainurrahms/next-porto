dd3332ac648c1cc3cd10652ae5dea8ed6a509f4b
Ainur Rahman <ainur.rahman@amartha.com>
2023-11-26 10:12:35 +0700

    feat: FEAT-001 Add section jumbotron and about me

---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import PP from "@assets/images/dummy-pp.png";
+import arrow from "@assets/images/arrow.svg";
+
+const images = {
+  PP,
+  arrow,
+};
+
+export default images;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      height:{
+        '85vh': '85vh'
       },
+      colors:{
+        'light-grey': '#EEE'
+      }
     },
   },
-  plugins: [],
 }
 export default config
