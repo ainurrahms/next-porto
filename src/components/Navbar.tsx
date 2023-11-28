9882b3f5b5f52292b7521e884b2701a07908e604
Ainur Rahman <ainur.rahman@amartha.com>
2023-11-28 23:54:02 +0700

    fix: FEAT-010 Fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Navbar = () => {
   return (
     <div className="mx-2% fixed bottom-0 left-0 right-auto top-0 mb-6 mt-6 flex items-start">
-      <div className="min-h-4.5rem flex h-auto flex-col items-center justify-between gap-y-4 rounded-full border border-gray-300 p-2 shadow-md">
+      <div className="min-h-4.5rem flex h-auto flex-col items-center justify-between gap-y-4 rounded-full border border-gray-300 bg-white p-2 shadow-md">
         <a href="#" className="transition duration-300 hover:scale-110">
           <div className="flex h-12 w-12 items-center justify-center rounded-full border border-solid border-gray-300 bg-white shadow-md">
             <Image src={images.PP} alt="linkedin-logo" width={50} height={50} />
